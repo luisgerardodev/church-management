54d00cbbcff9ef9a1b206905e2dcf59a3dc2a072
Malui <yaminolight@gmail.com>
2020-01-29 07:49:20 -0400

    added components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+import { Container } from '@material-ui/core';
 
 class App extends Component {
   render() {
     return (
     <div>
       <NavBar/>
+      <Container>
+        <h1>Church Management App</h1>
+      </Container>
     </div>
     )
   }
